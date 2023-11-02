1f4d854017ef996c210301bc6d9db6977b1340b6
Zgibbs58 <zacharywgibbs@gmail.com>
2023-11-02 00:59:07 -0500

    Updated the navLinks to close navMenu when clicked.

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,9 +1,9 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-export default function NavLink({ linkName, linkPath }) {
+export default function NavLink({ linkName, linkPath, onClick }) {
   return (
-    <Link to={linkPath} className="group md:text-2xl font-semibold transition duration-300 mx-2">
+    <Link to={linkPath} className="group md:text-2xl font-semibold transition duration-300 mx-2" onClick={onClick}>
       {linkName}
       <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-gray-500"></span>
     </Link>
@@ -13,4 +13,5 @@ export default function NavLink({ linkName, linkPath }) {
 NavLink.propTypes = {
   linkName: PropTypes.string.isRequired,
   linkPath: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
