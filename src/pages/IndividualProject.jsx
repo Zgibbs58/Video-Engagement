94034a8a7740cd3cd93ff79d4b1c6f3073cd19cd
Zach Gibbs <zacharywgibbs@gmail.com>
2023-11-16 22:32:38 -0600

    Added content to the AstronomME description data and broke it into chunks using an array of paragraphs.

--- src/pages/IndividualProject.jsx
+++ src/pages/IndividualProject.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { projectData } from "../../utils/projectData";
 import { useEffect } from "react";
+import SecondaryButton from "../components/SecondaryButton";
 
 export default function IndividualProject() {
   console.log(projectData);
@@ -19,8 +20,21 @@ export default function IndividualProject() {
     <div className="bg-gray-100 min-h-screen">
       <div className="max-w-7xl mx-auto py-12 sm:px-6 lg:px-8">
         <div className="max-w-3xl mx-auto">
-          <h1 className="text-3xl font-bold text-gray-900">{project.name}</h1>
-          <p className="mt-4 text-gray-500">{project.description}</p>
+          <h3 className="text-4xl font-bold">{project.name}</h3>
+          <p className="text-left text-xl font-semi-bold underline underline-gray-500">Project Description</p>
+          {project.fullDescription.map((paragraph) => (
+            <p className="text-left mt-4" key={paragraph + 1}>
+              {paragraph}
+            </p>
+          ))}
+          <p className="text-left text-xl font-semi-bold mt-4 underline underline-gray-500">Technologies Used</p>
+          <div className="mt-4 flex flex-wrap w-1/2">
+            {project.technologies.map((tech) => (
+              <span className="bg-gray-300 rounded-md px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mt-2" key={[tech] + 1}>
+                {tech}
+              </span>
+            ))}
+          </div>
           <img className="mt-8 rounded-lg shadow-lg" src={project.image} alt={project.name} />
         </div>
       </div>
