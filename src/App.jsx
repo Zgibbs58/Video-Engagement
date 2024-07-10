9d1ba9074e886fd863d6b8681017b824eddcfe8c
Zach Gibbs <zacharywgibbs@gmail.com>
2024-07-10 11:00:19 -0500

    added google analytics.

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,19 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { HelmetProvider } from "react-helmet-async";
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
 
 function App() {
+  useEffect(() => {
+    const trackingId = import.meta.env.VITE_GA_TRACKING_ID;
+    if (trackingId) {
+      ReactGA.initialize(trackingId);
+      ReactGA.send("pageview");
+    } else {
+      console.error("Google Analytics Tracking ID is missing");
+    }
+  }, []);
   return (
     <>
       <HelmetProvider>
