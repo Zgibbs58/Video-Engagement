7dc262c7c646dfdee58930a9f8f6f91d4b09ddfc
Zach Gibbs <zacharywgibbs@gmail.com>
2024-07-16 14:07:31 -0500

    Updated Variable name.

--- sendMail.js
+++ sendMail.js
@@ -20,7 +20,7 @@ const ses = new aws.SES({
   apiVersion: "2010-12-01",
   region: process.env.AWS_SES_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
